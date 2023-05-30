d47a09425999ee238511efca739535a76b95486f
ArdaEO14455 <14455@coderacademy.edu.au>
2023-05-30 17:39:53 +1000

    added card model with SQL alchemy

--- app.py
+++ app.py
@@ -1,6 +1,24 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://trello_dev:spammeggs123@localhost:5432/trello'
+
+db = SQLAlchemy(app)
+# print (db.__dict__)
+
+class Card(db.Model):
+    id = db.Column(db.Integer, primary_key=True) #This specifies the primary key
+    title = db.Column(db.String(100)) #the value next to string indicates max characters, if not specified, it will be 255. no need to specify no null, sqlalchemy will take care of that
+    description = db.Column(db.Text()) #text has no limit of characters
+    date = db.Column(db.Date)
+
+
+@app.cli.command('create')
+def create_db():
+    db.create_all()
+    print('Tables created successfully')
 
 @app.route('/')
 def index():
