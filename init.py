c5c8590a4dede8149c3d06ae6e598eb24db34af2
ArdaEO14455 <14455@coderacademy.edu.au>
2023-06-05 05:24:57 +1000

    Refactored Card Model

--- init.py
+++ init.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
 
 #Create instances of functionalities
-db = SQLAlchemy(app) #instance of SQL Alchemy to communicate with the database and provide commands from python
-ma = Marshmallow(app) #instance of Marshmallow, used to create schemas
-bcrypt = Bcrypt(app) #instance of bcrypt, used to encrypt and decrypt passwords
-jwt = JWTManager(app) #instance of the JWT (token) manager app
+db = SQLAlchemy() #instance of SQL Alchemy to communicate with the database and provide commands from python
+ma = Marshmallow() #instance of Marshmallow, used to create schemas
+bcrypt = Bcrypt() #instance of bcrypt, used to encrypt and decrypt passwords
+jwt = JWTManager() #instance of the JWT (token) manager app
