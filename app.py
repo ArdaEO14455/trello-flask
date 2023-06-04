200eaec9bfed31d0ee87768b28fe65cda77826d4
ArdaEO14455 <14455@coderacademy.edu.au>
2023-06-04 18:24:47 +1000

    "Updated Work"

--- app.py
+++ app.py
@@ -1,50 +1,86 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from datetime import date
+from flask_marshmallow import Marshmallow
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://trello_dev:spammeggs123@localhost:5432/trello'
+app.config[
+    "SQLALCHEMY_DATABASE_URI"
+] = "postgresql+psycopg2://trello_dev:spameggs123@localhost:5432/trello"
 
 db = SQLAlchemy(app)
-# print (db.__dict__)
+ma = Marshmallow(app)
+
 
 class Card(db.Model):
-    __tablename__ = 'cards'
-    
-    id = db.Column(db.Integer, primary_key=True) #This specifies the primary key
-    title = db.Column(db.String(100)) #the value next to string indicates max characters, if not specified, it will be 255. no need to specify no null, sqlalchemy will take care of that
-    description = db.Column(db.Text()) #text has no limit of characters
-    date = db.Column(db.Date)
+    __tablename__ = "cards"
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    description = db.Column(db.Text())
+    status = db.Column(db.String(30))
+    date_created = db.Column(db.Date())
+
+class CardSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'title', 'description', 'status', 'date_created')
 
 
-@app.cli.command('create')
+@app.cli.command("create")
 def create_db():
     db.drop_all()
-    db.create_all() #if a table that is already created is called to be created again, it will be ignored. its like an 'if not exists' statement.
-    print('Tables created successfully')
+    db.create_all()
+    print("Tables created successfully")
 
-@app.cli.command('seed')
+
+@app.cli.command("seed")
 def seed_db():
-    card = Card(
-        title = 'Start the Project',
-        description = 'Stage 1 - Create an ERD',
-        date = date.today()
-    )
+    # Create an instance of the Card model in memory
+    cards = [
+        Card(
+            title="Start the project",
+            description="Stage 1 - Create an ERD",
+            status="Done",
+            date_created=date.today(),
+        ),
+        Card(
+            title="ORM Queries",
+            description="Stage 2 - Implement several queries",
+            status="In Progress",
+            date_created=date.today(),
+        ),
+        Card(
+            title="Marshmallow",
+            description="Stage 3 - Implement jsonify of models",
+            status="In Progress",
+            date_created=date.today(),
+        ),
+    ]
 
-    #Truncate the Card Table
+    # Truncate the Card table
     db.session.query(Card).delete()
-    #add the card to the session (transaction)
-    db.session.add(card) #the 'session' acts like version control, whereby the entire block of code its embedded in needs to be successfully executed. if it partially works but something goes wrong, it rolls back.
 
-    #Commit the transaction to the database
-    db.session.commit() #This line then commits the transaction 
-    print('Models Seeded')
+    # Add the card to the session (transaction)
+    db.session.add_all(cards)
+
+    # Commit the transaction to the database
+    db.session.commit()
+    print("Models seeded")
 
 
-@app.route('/')
+@app.route('/cards')
+def all_cards():
+  # select * from cards;
+  stmt = db.select(Card).order_by(Card.status.desc())
+  cards = db.session.scalars(stmt).all()
+  return CardSchema(many=True).dumps(cards)
+
+
+@app.route("/")
 def index():
-    return 'hello world!'
+    return "Hello World!"
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
