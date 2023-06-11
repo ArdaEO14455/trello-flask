4d3a3d12517e2e45300baac6c12ee91a3e7a6067
ArdaEO14455 <14455@coderacademy.edu.au>
2023-06-11 21:38:48 +1000

    Added get, create, and update card functionality

--- blueprints/cards_bp.py
+++ blueprints/cards_bp.py
@@ -1,11 +1,12 @@
-from flask import Blueprint
+from flask import Blueprint, request
 from models.card import Card, CardSchema
 from init import db
 from flask_jwt_extended import jwt_required
 from blueprints.auth_bp import admin_required
+from datetime import date
 
 
-cards_bp = Blueprint('cards', __name__)
+cards_bp = Blueprint('cards', __name__, url_prefix='/cards')
 
 @cards_bp.route('/cards')
 @jwt_required()
@@ -16,3 +17,46 @@ def all_cards():
   stmt = db.select(Card).order_by(Card.status.desc())
   cards = db.session.scalars(stmt).all()
   return CardSchema(many=True).dump(cards)
+
+# Get one card
+@cards_bp.route('/<int:card_id>')
+def one_card(card_id):
+  stmt = db.select(Card).filter_by(id=card_id)
+  card = db.session.scalar(stmt)
+  if card:
+    return CardSchema().dump(card)
+  else:
+    return {'error': 'Card not found'}, 404
+  
+#Create a new card
+@cards_bp.route('/', methods=['POST'])
+def create_card():
+  #Load the incoming POST data via the schema
+  card_info = CardSchema().load(request.json)
+  # Create a new card instance from the card_info
+  card = Card(
+    title = card_info['title'],
+    description = card_info['description'],
+    status = card_info['status'],
+    date_created = date.today()
+  )
+  #add and commit the new card to the session
+  db.session.add(card)
+  db.session.commit()
+  #Send the new card back to the client
+  return CardSchema().dump(card), 201
+
+#Update Card
+@cards_bp.route('/<int:card_id>', methods=['PUT', 'PATCH'])
+def update_card(card_id):
+  stmt = db.select(Card).filter_by(id=card_id)
+  card = db.session.scalar(stmt)
+  card_info = CardSchema().load(request.json)
+  if card:
+    card['title'] = card_info.get['title', card.title],
+    card['description'] = card_info.get['description', card.description],
+    card['status'] = card_info.get['status', card.status],
+    db.session.commit()
+    return CardSchema().dump(card)
+  else:
+    return {'error': 'Card not found'}, 404
